6d8ad15b1e67f59dd914dd1f9e8cd0fba93d7f78
James Aules <james_aules@icloud.com>
2023-05-06 11:19:19 -0400

    solution 4

--- solution4.py
+++ solution4.py
@@ -1,3 +1,12 @@
 # Write code for algorithm 4 below
 # Write a function that calculates the value of 'a' to the power of 'b'.
 
+def power(a, b):
+    if b < 0:
+        print('Not Valid')
+    elif b == 1:
+        return a
+    else:
+        return a * power(a, b -1)
+
+print(power(2,3))
