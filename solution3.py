8c318ca6ea73c27612487db6cfefca54980ecc40
James Aules <james_aules@icloud.com>
2023-05-06 11:18:53 -0400

    solution 3

--- solution3.py
+++ solution3.py
@@ -1,4 +1,19 @@
 # Write code for algorithm 3 below
 #Write a function that returns the nth elements in the Fibonacci Sequence.
 
-def fibonacci():
+def fibonacci(num):
+    if num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    elif num <= 0:
+        return 0
+    else:
+        return fibonacci(num -1) + fibonacci(num -2)
+    
+print("2nd fib number:")
+print(fibonacci(2))
+print("4th fib number:")
+print(fibonacci(4))
+print("8th fib number:")
+print(fibonacci(8))
--- solution4.py
+++ solution4.py
@@ -1,1 +1,3 @@
 # Write code for algorithm 4 below
+# Write a function that calculates the value of 'a' to the power of 'b'.
+
