3a41c7f434e1f0b0a69c8c410d18dd6211411a61
James Aules <james_aules@icloud.com>
2023-05-06 11:03:48 -0400

    solution 2

--- solution1.py
+++ solution1.py
@@ -1,2 +1,11 @@
 # Write code for algorithm 1 below
 # 1. Write a program that takes a positive number as an argument and prints the numbers from n to zero.
+
+def countDown(num):
+    if num <= 0:
+        return 0
+    else:
+        print(num)
+        return countDown(num-1)
+
+print(countDown(5))
--- solution2.py
+++ solution2.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 2 below
+# 2. Write a function that prints the natural numbers from 1 to n.
+
+def NaturalN(x, i=1):
+   
+    if i > x:
+        return 0
+    else:
+        print(i)
+        NaturalN(x, i+1)
+    
+print(NaturalN(5))
